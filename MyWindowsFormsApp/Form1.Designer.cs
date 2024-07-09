0de2aae3baf926d739a9bf025d418503b827e1e0
Dede <154935734+rhoda-lee@users.noreply.github.com>
2024-07-09 11:11:21 +0000

    login screen done

--- MyWindowsFormsApp/Form1.Designer.cs
+++ MyWindowsFormsApp/Form1.Designer.cs
@@ -1,16 +1,9 @@
-﻿namespace MyWindowsFormsApp
+﻿namespace LoginScreenApp
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,21 +13,68 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            this.textBoxUsername.Location = new System.Drawing.Point(115, 28);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(170, 22);
+            this.textBoxUsername.TabIndex = 0;
+
+            this.textBoxPassword.Location = new System.Drawing.Point(115, 66);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(170, 22);
+            this.textBoxPassword.TabIndex = 1;
+
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(34, 31);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(75, 17);
+            this.labelUsername.TabIndex = 2;
+            this.labelUsername.Text = "Username:";
+
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(34, 69);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(73, 17);
+            this.labelPassword.TabIndex = 3;
+            this.labelPassword.Text = "Password:";
+
+            this.buttonLogin.Location = new System.Drawing.Point(115, 104);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 4;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(317, 151);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
-        #endregion
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Button buttonLogin;
     }
+}
+
 }
 
--- MyWindowsFormsApp/Form1.cs
+++ MyWindowsFormsApp/Form1.cs
@@ -1,20 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace MyWindowsFormsApp
+namespace LoginScreenApp
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string username = textBoxUsername.Text;
+            string password = textBoxPassword.Text;
+            MessageBox.Show($"Username: {username}\nPassword: {password}");
         }
     }
 }
