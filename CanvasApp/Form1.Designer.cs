bc30ab0f69d0e3f33c40cbb5469be1a913e9d62b
Dede <154935734+rhoda-lee@users.noreply.github.com>
2024-07-09 11:20:21 +0000

    canvas app done

---
+++ CanvasApp/Form1.Designer.cs
@@ -0,0 +1,33 @@
+﻿namespace DrawingApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "Drawing Application";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+        }
+    }
+}
+
+
---
+++ CanvasApp/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawingApp
+{
+    public partial class Form1 : Form
+    {
+        private bool isDrawing = false;
+        private Point startPoint = Point.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrawing = true;
+                startPoint = e.Location;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                using (Graphics g = this.CreateGraphics())
+                {
+                    g.DrawLine(Pens.Black, startPoint, e.Location);
+                }
+                startPoint = e.Location;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrawing = false;
+            }
+        }
+    }
+}
